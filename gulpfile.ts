ddb8f3765534c176f8ac16a27b2dc23195e66a2d
iamayomi <aayomidesherif2019@gmail.com>
2025-05-10 00:08:58 +0100

    fixes: gulp file

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,15 +1,14 @@
-import * as gulp from "gulp";
-
+import * as gulp from 'gulp';
 
 // Task to copy files from public folder
- function copyPublic() {
-  return gulp.src("src/public/**/*.*").pipe(gulp.dest("dist/src/public"));
-};
+function copyPublic() {
+  return gulp.src('src/public/**/*.*').pipe(gulp.dest('dist/src/public'));
+}
 
 // Task to copy files from views folder
-function copyViews () {
-  return gulp.src("src/views/**/*.*").pipe(gulp.dest("dist/src/views"));
-};
+function copyViews() {
+  return gulp.src('src/views/**/*.*').pipe(gulp.dest('dist/src/views'));
+}
 
 // Default task
 
